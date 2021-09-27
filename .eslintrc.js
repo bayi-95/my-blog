558c724357ed4bd96fba986f5c0f81619296d47c
申光普 <necro.vice@yahoo.com>
2021-09-27 13:25:21 +0800

    chore: 配置代码格式化

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'@vue/typescript',
+		'eslint:recommended',
+		'plugin:vue/essential',
+		'plugin:prettier/recommended',
+	],
+	globals: {
+		NativeSupportApi: true,
+		BrowserApi: true,
+	},
+	rules: {
+		'prettier/prettier': 'warn',
+		'no-console': 'off',
+		'no-irregular-whitespace': 'off',
+		'no-undef': 'off',
+		'no-redeclare': 'off',
+		'no-useless-escape': 'off',
+		'no-debugger': 'warn',
+	},
+	parserOptions: {
+		parser: '@typescript-eslint/parser',
+	},
+}
