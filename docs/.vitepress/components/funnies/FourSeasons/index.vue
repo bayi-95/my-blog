b8b6ceae2d0cb24ae076b9380fb0f4f9e5b4f0b8
shenguangpu <shenguangpu@oureman.com>
2024-02-04 13:34:07 +0800

    feat: 添加动态的动画主题

--- docs/.vitepress/components/funnies/FourSeasons/index.vue
+++ docs/.vitepress/components/funnies/FourSeasons/index.vue
@@ -1,58 +1,59 @@
 <template>
-    <component :is="component" />
+  <component :is="component" />
 </template>
 
 <script lang="ts">
 import SakuraFlying from './modules/SakuraFlying.vue'
 import Sunshine from './modules/Sunshine.vue'
+import SnowFlying from './modules/SnowFlying.vue'
 import { defineComponent, markRaw } from 'vue'
 
 const SEASON_MAP = {
-    spring: SakuraFlying,
-    summer: Sunshine,
-    autumn: Sunshine,
-    winter: 'winter'
+  spring: SakuraFlying,
+  summer: Sunshine,
+  autumn: Sunshine,
+  winter: SnowFlying
 } as Record<string, any>
 
 export default defineComponent({
-    name: 'SeasonsComps',
-    data() {
-        return {
-            component: 'SakuraFlying',
-            season: ''
-        }
-    },
-    created() {
-        this.component = markRaw(SEASON_MAP[this.judgeSeason()])
-    },
-    methods: {
-        judgeSeason(): string {
-            const month = new Date().getMonth() + 1
-            let result = ''
-            switch (month) {
-                case 3:
-                case 4:
-                case 5:
-                    result = 'spring'
-                    break
-                case 6:
-                case 7:
-                case 8:
-                    result = 'summer'
-                    break
-                case 9:
-                case 10:
-                case 11:
-                    result = 'autumn'
-                    break
-                case 12:
-                case 1:
-                case 2:
-                    result = 'winter'
-                    break
-            }
-            return result
-        }
+  name: 'SeasonsComps',
+  data() {
+    return {
+      component: 'SakuraFlying',
+      season: ''
     }
+  },
+  created() {
+    this.component = markRaw(SEASON_MAP[this.judgeSeason()])
+  },
+  methods: {
+    judgeSeason(): string {
+      const month = new Date().getMonth() + 1
+      let result = ''
+      switch (month) {
+        case 3:
+        case 4:
+        case 5:
+          result = 'spring'
+          break
+        case 6:
+        case 7:
+        case 8:
+          result = 'summer'
+          break
+        case 9:
+        case 10:
+        case 11:
+          result = 'autumn'
+          break
+        case 12:
+        case 1:
+        case 2:
+          result = 'winter'
+          break
+      }
+      return result
+    }
+  }
 })
 </script>
---
+++ docs/.vitepress/components/funnies/FourSeasons/modules/SnowFlying.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SnowFlying',
+  mounted() {
+    import('../utils/snow-flying').then(() => {
+      new Snowflakes({
+        color: '#efefef'
+      })
+    })
+  }
+})
+</script>
