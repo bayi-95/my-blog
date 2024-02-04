a37ad82b502e8567172d06d97330ae7286619e86
shenguangpu <shenguangpu@oureman.com>
2024-02-04 14:16:52 +0800

    chore(deps): 更新依赖 & 使用 pnpm workspace 管理 Monorepo 架构

---
+++ packages/scripts/src/config/index.ts
@@ -0,0 +1,28 @@
+import process from 'node:process';
+import { loadConfig } from 'c12';
+import type { CliOption } from '../types';
+
+const defaultOptions: CliOption = {
+  cwd: process.cwd(),
+  cleanupDirs: [
+    '**/dist',
+    '**/package-lock.json',
+    '**/yarn.lock',
+    '**/pnpm-lock.yaml',
+    '**/node_modules',
+    '!node_modules/**'
+  ],
+  ncuCommandArgs: ['--deep', '-u'],
+};
+
+export async function loadCliOptions(overrides?: Partial<CliOption>, cwd = process.cwd()) {
+  const { config } = await loadConfig<Partial<CliOption>>({
+    name: 'soybean',
+    defaults: defaultOptions,
+    overrides,
+    cwd,
+    packageJson: true
+  });
+
+  return config as CliOption;
+}
