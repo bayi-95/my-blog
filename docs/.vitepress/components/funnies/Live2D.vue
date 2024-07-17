3be91cdf4a21f7fbeb708b5f5e7c575898128a29
shenguangpu <shenguangpu@oureman.com>
2024-07-17 14:23:02 +0800

    feat: 更新 live2d 模型资源

--- docs/.vitepress/components/funnies/Live2D.vue
+++ docs/.vitepress/components/funnies/Live2D.vue
@@ -1,16 +1,60 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import type { ModelOptions } from 'oh-my-live2d'
 
 export default defineComponent({
   name: 'Live2D',
   async mounted() {
     const { loadOml2d } = await import('oh-my-live2d')
+    const modelNames = [
+      'HK416-1-normal',
+      'HK416-2-destroy',
+      'HK416-2-normal',
+      'Kar98k-normal',
+      'Pio',
+      'Senko_Normals',
+      'bilibili-22',
+      'bilibili-33',
+      'cat-black',
+      'cat-white',
+      'chino',
+      'date',
+      'hallo',
+      'haruto',
+      'hibiki',
+      'histoire',
+      'kobayaxi',
+      'koharu',
+      'kp31',
+      'live_uu',
+      'mai',
+      'murakumo',
+      'platelet',
+      'platelet_2',
+      'potion-Maker-Pio',
+      'rem',
+      'rem_2',
+      'shizuku',
+      'shizuku_48',
+      'shizuku_pajama',
+      'terisa',
+      'tia',
+      'umaru',
+      'uni',
+      'wed_16',
+      'xisitina',
+      'z16'
+    ]
+    const models: ModelOptions[] = modelNames.map((name) => ({
+      path: `https://model.oml2d.com/${name}/model.json`,
+      position: [0, 60],
+      scale: 0.08,
+      stageStyle: {
+        height: 350
+      }
+    }))
     loadOml2d({
-      models: [
-        {
-          path: 'https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json'
-        }
-      ]
+      models
     })
   }
 })
